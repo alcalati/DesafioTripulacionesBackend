342fcf7cfa3b4928fe46eb39cd71c9c8141d3a97
paulotorrente <paulotorrentebr@gmail.com>
2024-09-06 19:32:55 +0200

    Prueba

---
+++ utils/emailService.js
@@ -0,0 +1,20 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendPasswordResetEmail = async (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
