fdb8be7cb667319411709c1e4d37eab5f8f12f75
alba-garcia09 <albagarlaz@gmail.com>
2024-09-14 10:37:49 +0200

    change logo to charge

--- src/api/partners/partners.model.js
+++ src/api/partners/partners.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const partnerSchema = new mongoose.Schema({
   name: { type: String, required: true, },
   description: { type: String, required: true, },
-  logo: { type: String, required: true, }, // URL de la imagen
+  charge: { type: String, required: true, },
 });
 
 const partnerModel = mongoose.model('Partner', partnerSchema, 'partner');
