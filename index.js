a12c5376c74c5a3779f0f0e3598d9c7ecf8ca8f0
alcalati <alcaat@edem.es>
2024-09-16 18:45:13 +0200

    fixed cosas

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
-import apiRouter from './src/api/router.js';
 import './database.js';
+import apiRouter from './src/api/router.js';
 
 const server = express();
 server.use(express.json());
 server.use(cors({ origin: true }));
 
 // Rutas de autenticación
-server.use('/api/auth', authRoutes);
+server.use('/auth', authRoutes); // Esto debería funcionar
 
-// Rutas API
-server.use('/api', apiRouter);
+// Asegúrate de que apiRouter no esté interfiriendo
+server.use(apiRouter);
 
 // Puerto y escucha del servidor
 const { PORT } = process.env;
 const port = PORT || 3000;
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
